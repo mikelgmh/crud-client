1490d48b363cf2d36698da26e2240d0b8dd28aa8
Aketza19 <aketzae5@gmail.com>
2021-01-17 19:06:56 +0100

    Validation and comboBox

--- src/crudclient/controllers/ProductController.java
+++ src/crudclient/controllers/ProductController.java
@@ -5,34 +5,26 @@
  */
 package crudclient.controllers;
 
+import crudclient.client.CompanyRESTClient;
 import crudclient.client.ProductRESTClient;
+import crudclient.interfaces.CompanyInterface;
 import crudclient.interfaces.ProductInterface;
 import crudclient.model.Company;
 import crudclient.model.Product;
-import crudclient.model.User;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.ResourceBundle;
-import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.ReadOnlyBooleanWrapper;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.ObservableSet;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -40,12 +32,9 @@ import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
-import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.util.converter.DoubleStringConverter;
 import javafx.util.converter.FloatStringConverter;
@@ -61,8 +50,6 @@ public class ProductController {
     private Stage stage;
     @FXML
     private TableView<Product> tv_Tabla;
-   // @FXML
-   // private TableColumn<Product, Integer> tc_Id;
     @FXML
     private TableColumn<Product, String> tc_Name;
     @FXML
@@ -77,22 +64,17 @@ public class ProductController {
     private Button btn_OrderCreate;
     @FXML
     private Button btn_Filter;
-   /* @FXML
-    private TextField tf_Id;
-    @FXML
-    private TextField tf_Name;
-    @FXML
-    private TextField tf_Weight;
-    @FXML
-    private TextField tf_Price;*/
     @FXML
     private Label lbl_Choose;
     @FXML
-    private ComboBox<Company> combo_Company;    
+    private ComboBox<Company> combo_Company;
     private static final Logger LOG = Logger.getLogger(ProductController.class.getName());
     private TableView<ObservableList<StringProperty>> table = new TableView<>();
     private ObservableList<Product> pr;
+    private ObservableList<Company> co;
     private ProductInterface productImplementation;
+    private CompanyInterface companyImplementation;
+    
 
     /**
      * Initializes the controller class.
@@ -113,34 +95,38 @@ public class ProductController {
         stage.setTitle("Product Managment");
         stage.setResizable(false);
         LOG.log(Level.INFO, "Stage ");
-        /*En los dos labels se muestran datos que hemos recibido desde el lado
-        servidor*/
-       /* tf_Id.setTooltip(new Tooltip("ID of the product"));
-        tf_Name.setTooltip(new Tooltip("Name of the product"));
-        tf_Weight.setTooltip(new Tooltip("Weight of the product"));
-        tf_Price.setTooltip(new Tooltip("Price of the product"));*/
+        
         btn_Create.setTooltip(new Tooltip("Create a new product"));
+        
         btn_Delete.setTooltip(new Tooltip("Delete a product"));
+        
         btn_Delete.setOnAction(this::handleOnClickDelete);
+        
         btn_OrderCreate.setTooltip(new Tooltip("Create an order"));
+        
         btn_Filter.setTooltip(new Tooltip("Create an order"));
-        //this.pr = FXCollections.observableSet(getProductImplementation().findAllProducts_XML(new GenericType<Set<Product>>() { }));
+        
         tv_Tabla.getSelectionModel().setSelectionMode(
                 SelectionMode.MULTIPLE
         );
+        
+        combo_Company.setItems(comboCompany());
+        
         LOG.log(Level.INFO, "Tooltip ");
+        
         tabla();
+        
         LOG.log(Level.INFO, "tabla ");
+        
         tableInfo();
+        
         LOG.log(Level.INFO, "tablainfo ");
+        
         stage.show();
-
     }
 
     private void tabla() {
         tv_Tabla.setEditable(true);
-
-       // tc_Id.setCellValueFactory(new PropertyValueFactory<>("id"));
 
         tc_Name.setCellValueFactory(new PropertyValueFactory<>("name"));
 
@@ -151,51 +137,77 @@ public class ProductController {
         tc_Name.setCellFactory(TextFieldTableCell.forTableColumn());
         tc_Name.setOnEditCommit((TableColumn.CellEditEvent<Product, String> data) -> {
             LOG.log(Level.SEVERE, "New Name: ", data.getNewValue());
-            // System.out.println("Nuevo Nombre: " + data.getNewValue());
-            // System.out.println("Antiguo Nombre: " + data.getOldValue());
             LOG.log(Level.SEVERE, "Old Name: ", data.getOldValue());
+            if (!Pattern.matches("[a-zA-Z0-9]+", data.getNewValue())) {
+                Alert alert = new Alert(AlertType.WARNING);
+                alert.setTitle("Warning Dialog");
+                alert.setHeaderText("Look, a Warning Dialog");
+                alert.setContentText("Careful with the next step!");
+
+                alert.showAndWait();
+                tv_Tabla.refresh();
+
+            } else {
             Product p = data.getRowValue();
             p.setName(data.getNewValue());
+            }
         });
 
-        //tc_Weight.setCellFactory(TextFieldTableCell.forTableColumn());
-        // TableColumn<Product, Float> tc_Weight = new TableColumn<>("Weight");
         tc_Weight.setCellValueFactory(new PropertyValueFactory<Product, Float>("weight"));
         FloatStringConverter converterFloat = new FloatStringConverter();
         tc_Weight.setCellFactory(TextFieldTableCell.<Product, Float>forTableColumn(converterFloat));
         tc_Weight.setOnEditCommit(data -> {
+            if (!Pattern.matches("[a-zA-Z0-9]+", data.getNewValue().toString())) {
+                Alert alert = new Alert(AlertType.WARNING);
+                alert.setTitle("Warning Dialog");
+                alert.setHeaderText("Look, a Warning Dialog");
+                alert.setContentText("Careful with the next step!");
+                alert.showAndWait();
+                tv_Tabla.refresh();
+
+            } else {
             data.getRowValue().setWeight(data.getNewValue());
+            }
         });
 
         tc_Price.setCellValueFactory(new PropertyValueFactory<Product, Double>("price"));
         DoubleStringConverter converterDouble = new DoubleStringConverter();
         tc_Price.setCellFactory(TextFieldTableCell.<Product, Double>forTableColumn(converterDouble));
         tc_Price.setOnEditCommit(data -> {
-            data.getRowValue().setPrice(data.getNewValue());
-        });
+            
+              if (!Pattern.matches("[a-zA-Z0-9]+", data.getNewValue().toString())) {
+                Alert alert = new Alert(AlertType.WARNING);
+                alert.setTitle("Warning Dialog");
+                alert.setHeaderText("Look, a Warning Dialog");
+                alert.setContentText("Careful with the next step!");
+                alert.showAndWait();
+                tv_Tabla.refresh();
 
+            } else {
+            data.getRowValue().setPrice(data.getNewValue());
+              }
+        });
     }
+    
+
+    
 
     private void tableInfo() {
-
         LOG.log(Level.INFO, "pr ");
         ProductRESTClient rest = new ProductRESTClient();
         pr = FXCollections.observableArrayList(rest.findAllProducts_XML(new GenericType<List<Product>>() {
         }));
-
         tv_Tabla.setItems(pr);
-        // this.pr = FXCollections.observableSet(getProductImplementation().findAllProducts_XML(new GenericType<List<Product>>() {
-        //}));
-        //this.table.setItems(pr);
-        /*  Product product = new Product();
-        User user = new User();
-        product.setId(1);
-        product.setName("Aketza");
-        product.setWeight(50);
-        product.setPrice(300);
-        product.setUser(user);
-        ObservableList<Product> info = FXCollections.observableArrayList(product);
-        tv_Tabla.setItems(info);*/
+    }
+    private ObservableList<Company> comboCompany(){
+       CompanyRESTClient rest = new CompanyRESTClient();
+
+       ObservableList<Company> types = FXCollections.observableArrayList(rest.getCompanies());
+       
+       return types;
+    }
+    
+    private void edit() {
 
     }
 
@@ -213,8 +225,11 @@ public class ProductController {
 
         // get last row
         int row = table.getItems().size() - 1;
-        table.getSelectionModel().select(row, pos.getTableColumn());
-
+        tv_Tabla.getFocusModel().focus(pos);
+        tv_Tabla.requestFocus();
+        //table.getSelectionModel().select(row, pos.getTableColumn());
+        // tv_Tabla.getFocusModel().focus(row, tableColumn);
+        // tv_Tabla.requestFocus();
         // scroll to new row
         tv_Tabla.scrollTo(product);
 
@@ -223,16 +238,17 @@ public class ProductController {
     @FXML
     private void handleOnClickDelete(ActionEvent event) {
         btn_Delete.setDisable(false);
-        //tv_Tabla.getItems().remove(tv_Tabla.getSelectionModel().getSelectedItem());
         ProductRESTClient rest = new ProductRESTClient();
         rest.remove(tv_Tabla.getSelectionModel().getSelectedItem().getId().toString());
         tableInfo();
     }
 
+    
     @FXML
     private void handleOnClickFilter(ActionEvent event) {
     }
 
+    
     @FXML
     private void handleOnClickCreateOrder(ActionEvent event) {
         List<String> names = tv_Tabla.getSelectionModel().getSelectedItems().stream()
@@ -240,11 +256,22 @@ public class ProductController {
                 .collect(Collectors.toList());
     }
 
+    
     public void setProductImplementation(ProductInterface product) {
         this.productImplementation = product;
     }
 
+    
     public ProductInterface getProductImplementation() {
         return this.productImplementation;
+    }
+    
+     public void setCompanyImplementation(CompanyInterface company) {
+        this.companyImplementation = company;
+    }
+
+     
+    public CompanyInterface getCompanyImplementation() {
+        return this.companyImplementation;
     }
 }
