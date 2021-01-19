7f4482bfe98f28033a8d85d476faaef00ac07498
Imanol Montoro <56909426+moxarlos@users.noreply.github.com>
2021-01-19 09:26:38 +0100

    Inicializar ventana Product

--- src/crudclient/controllers/OrderManagementController.java
+++ src/crudclient/controllers/OrderManagementController.java
@@ -6,6 +6,7 @@ package crudclient.controllers;
  */
 import crudclient.client.OrderRESTClient;
 import crudclient.factories.OrderFactory;
+import crudclient.factories.ProductFactory;
 import crudclient.model.Order;
 import crudclient.model.OrderStatus;
 import crudclient.model.User;
@@ -39,10 +40,13 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import javax.ws.rs.core.GenericType;
 import crudclient.interfaces.OrderInterface;
+import crudclient.interfaces.ProductInterface;
+import java.io.IOException;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 
 /**
  * FXML Controller class
@@ -102,8 +106,7 @@ public class OrderManagementController {
         stage.setTitle("Order Management");
         stage.setResizable(false);
         stage.setOnShowing(this::handleWindowsShowing);
-        
-       
+
         column_date.setSortType(TableColumn.SortType.DESCENDING);
         combo_statusOrder.setItems(FXCollections.observableArrayList(OrderStatus.values()));
         column_ID.setCellValueFactory(new PropertyValueFactory("id"));
@@ -111,21 +114,23 @@ public class OrderManagementController {
         column_totalPrice.setCellValueFactory(new PropertyValueFactory("total_price"));
         column_status.setCellValueFactory(new PropertyValueFactory("status"));
         column_user.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getUser().getName().concat(" " + cellData.getValue().getUser().getSurname())));
-        orderData=FXCollections.observableArrayList(getOrderImplementation().findAllOrders(new GenericType<Set<Order>>() {}));
+        orderData = FXCollections.observableArrayList(getOrderImplementation().findAllOrders(new GenericType<Set<Order>>() {
+        }));
         FilteredList<Order> filteredData = new FilteredList<>(orderData, p -> true);
-        
+
         tableOrder.setItems(orderData);
         stage.show();
         logger.log(Level.INFO, "OrderManagement stage loaded.");
     }
 
-
     private void handleWindowsShowing(WindowEvent event) {
         txt_IDOrder.requestFocus();
+        btn_newOrder.setOnAction(this::handlerCreateOrder);
+        btn_newOrder.disableProperty().bind(Bindings.isEmpty(tableOrder.getSelectionModel().getSelectedItems()));
         btn_deleteOrder.setOnAction(this::handlerDeleteOrder);
         btn_deleteOrder.disableProperty().bind(Bindings.isEmpty(tableOrder.getSelectionModel().getSelectedItems()));
     }
-    
+
     public void setStage(Stage stage) {
         this.stage = stage;
     }
@@ -137,7 +142,7 @@ public class OrderManagementController {
     public void setOrderImplementation(OrderInterface order) {
         this.orderImplementation = order;
     }
-    
+
     public OrderInterface getOrderImplementation() {
         return this.orderImplementation;
     }
@@ -147,7 +152,21 @@ public class OrderManagementController {
         this.getOrderImplementation().removeOrder(order.getId().toString());
         tableOrder.getItems().remove(order);
     }
-    
 
+    private void handlerCreateOrder(ActionEvent event) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/crudclient/view/product.fxml"));
+        Parent root;
+        try {
+            root = (Parent) loader.load();
+            ProductController pController = ((ProductController) loader.getController());
+            ProductFactory productFactory = new ProductFactory();
+            ProductInterface product = productFactory.getImplementation();
+            pController.setProductImplementation(product);
+            pController.initStage(root);
+            this.stage.close();
+        } catch (IOException ex) {
+            Logger.getLogger(OrderManagementController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
 }
