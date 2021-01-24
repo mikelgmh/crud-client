7d88016d1fe0cb5b28d57e5b084bd0cdcddddea1
Imanol Montoro <imanolma90@gmail.com>
2021-01-24 22:13:45 +0100

    Suprimir Order
    Evitar problema en bucle al crear orden

--- src/crudclient/model/OrderProduct.java
+++ src/crudclient/model/OrderProduct.java
@@ -20,7 +20,6 @@ public class OrderProduct implements Serializable {
 
     private OrderProductId id;
     private Product product;
-    private Order order;
     private Float total_price;
     private Integer total_quantity;
 
@@ -44,15 +43,6 @@ public class OrderProduct implements Serializable {
 
     public void setProduct(Product product) {
         this.product = product;
-    }
-    
-    @XmlElement(name="order")
-    public Order getOrder() {
-        return order;
-    }
-
-    public void setOrder(Order order) {
-        this.order = order;
     }
     
     @XmlElement(name="total_price")
